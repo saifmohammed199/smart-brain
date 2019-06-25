2259434de2193578236ff2ece670f5bd6b044115
Saif Mohammed <saifmohammedbloger@gmail.com>
2019-06-25 15:47:34 +0530

    created Rank and ImageLinkForm components, installed Particles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions={
+  particles:{
+    number:{
+      value:30,
+      density:{
+        enable:true,
+        value_area:80
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' params={particlesOptions} />
       <Navigation/>
         <Logo/>
-        {/*<ImageLinkForm/>
-        <FaceRecognition/>*/}
+        <Rank/>
+        <ImageLinkForm/>
+        {/*<FaceRecognition/>*/}
     </div>
   );
 }
