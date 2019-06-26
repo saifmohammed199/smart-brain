17bfc1a7f571b858d3f33c777554a7391b963924
Saif Mohammed <saifmohammedbloger@gmail.com>
2019-06-26 13:07:51 +0530

    Created FaceRecognition component and updated app component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
@@ -28,15 +29,19 @@ class App extends Component {
     super();
     this.state={
       input:'',
+      imageUrl:''
     }
   }
 
   onInputChange=(event)=>{
-    console.log(event.target.value);
+    this.setState({input:event.target.value});
   }
   onButtonSubmit=()=>{
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", 
-    "https://samples.clarifai.com/face-det.jpg")
+    this.setState({imageUrl:this.state.input})
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL, 
+    this.state.input)
     .then(
       function(response) {
         // do something with response
@@ -57,7 +62,7 @@ class App extends Component {
         <ImageLinkForm 
         onInputChange={this.onInputChange}
         onButtonSubmit={this.onButtonSubmit}/>
-        {/*<FaceRecognition/>*/}
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
     </div>
   )
 }
