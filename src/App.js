c465cec201fce7e2aa096e27ddb80917f0a98395
Saif Mohammed <saifmohammedbloger@gmail.com>
2019-06-26 12:15:02 +0530

    added  functionality onChange, Onclick in ImageLinkForm, installed clarifai and generated api kye

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: 'a43c8a86e7d7491184858ec949c224c9'
+ });
 
 const particlesOptions={
   particles:{
@@ -18,17 +23,44 @@ const particlesOptions={
   }
 }
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input:'',
+    }
+  }
+
+  onInputChange=(event)=>{
+    console.log(event.target.value);
+  }
+  onButtonSubmit=()=>{
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", 
+    "https://samples.clarifai.com/face-det.jpg")
+    .then(
+      function(response) {
+        // do something with response
+        console.log(response)
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+  render(){
   return (
     <div className="App">
       <Particles className='particles' params={particlesOptions} />
       <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm 
+        onInputChange={this.onInputChange}
+        onButtonSubmit={this.onButtonSubmit}/>
         {/*<FaceRecognition/>*/}
     </div>
-  );
+  )
+}
 }
 
 export default App;
